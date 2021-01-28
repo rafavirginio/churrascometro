7d4ae4c42b142d6aa49b9dac6d21bc7b2f5c3d1d
Rafa Virginio <rafael5845@gmail.com>
2021-01-28 14:31:18 -0300

    primeiro teste js

--- script.js
+++ script.js
@@ -4,3 +4,27 @@
 // Refrigerante/agua - 1000 ml por pessoa + de 6 horas 1500 ml
 
 // Crianças valem por 0,5
+
+let inputAdultos = document.getElementById("adultos")
+let inputCriancas = document.getElementById("criancas")
+let inputDuracao = document.getElementById("duracao")
+
+let resultado = document.getElementById("resultado")
+
+function calcular() {
+  console.log("Calculando...")
+  let adultos = inputAdultos.value
+  let criancas = inputCriancas.value
+  let duracao = inputDuracao.value
+
+  let qtdTotalCarne = carnePP(duracao) * adultos + (carnePP(duracao) / 2) * criancas
+  console.log(qtdTotalCarne)
+}
+
+function carnePP(duracao) {
+    if (duracao >= 6) {
+        return 650
+    } else {
+        return 400
+    }
+}

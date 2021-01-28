1005a6b6fd8b9429f69687055d9cbd56b7525f8e
Rafa Virginio <rafael5845@gmail.com>
2021-01-28 15:00:02 -0300

    update churrascometro

--- script.js
+++ script.js
@@ -18,7 +18,13 @@ function calcular() {
   let duracao = inputDuracao.value
 
   let qtdTotalCarne = carnePP(duracao) * adultos + (carnePP(duracao) / 2) * criancas
-  console.log(qtdTotalCarne)
+  let qtdTotalBebidas = bebidasPP(duracao) * adultos + (bebidasPP(duracao) / 2) * criancas
+  let qtdTotalCerveja = cervejaPP(duracao) * adultos 
+
+  resultado.innerHTML = `<p>${qtdTotalCarne/1000}Kg de Carne</p>`
+  resultado.innerHTML += `<p>${Math.ceil(qtdTotalCerveja/355)}Latas de Cerveja</p>`
+  resultado.innerHTML += `<p>${Math.ceil(qtdTotalBebidas)/2000}Garrafas 2L de Bebidas</p>`
+  
 }
 
 function carnePP(duracao) {
@@ -26,5 +32,20 @@ function carnePP(duracao) {
         return 650
     } else {
         return 400
+    }
+}
+
+function cervejaPP(duracao) {
+    if (duracao >= 6) {
+        return 2000
+    } else {
+        return 1200
+    }
+}
+function bebidasPP(duracao) {
+    if (duracao >= 6) {
+        return 1500
+    } else {
+        return 1000
     }
 }

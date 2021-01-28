5249aa11e3d8270b132dd478a335e3b2a5e44700
Rafa Virginio <rafael5845@gmail.com>
2021-01-28 13:10:15 -0300

    commit html e css

---
+++ script.js
@@ -0,0 +1,6 @@
+// Levantamento de requisitos
+// Carne - 400 ml gr por pessoa + de 6 horas - 650
+// Cerveja - 1200 ml por pessoa + de 6 horas - 2000 ml
+// Refrigerante/agua - 1000 ml por pessoa + de 6 horas 1500 ml
+
+// Crianças valem por 0,5
